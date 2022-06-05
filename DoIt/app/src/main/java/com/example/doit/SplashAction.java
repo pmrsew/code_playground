f023d64fcd0fd1b2c3262d3f9bea2465afc81e14
Paige <paige0011@gmail.com>
2022-06-05 14:35:19 -0500

    Implemented splash screen

--- DoIt/app/src/main/java/com/example/doit/SplashAction.java
+++ DoIt/app/src/main/java/com/example/doit/SplashAction.java
@@ -2,7 +2,9 @@ package com.example.doit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashAction extends AppCompatActivity {
 
@@ -10,5 +12,26 @@ public class SplashAction extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_action);
+
+        /* PART THREE BEGINS HERE, CODE ABOVE ALREADY GENERATED IN PREVIOUS PART */
+
+        //We are beginning by implementing Splash Screen
+
+        //Hide bar along top of screen so that the splash screen is full screen
+        getSupportActionBar().hide();
+
+        //Establish the activity following the splash screen popping up
+        final Intent i = new Intent(SplashAction.this,MainActivity.class);
+
+        //The code that pops splash screen on run for specified time before
+        //loading the main activity
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(i);
+                finish();
+            }
+        }, 1000); // = 1 second
+
     }
 }
