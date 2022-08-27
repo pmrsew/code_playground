ec4b469c231b74f2297f303310ce8d91abf6a929
Paige <paige0011@gmail.com>
2022-08-26 20:21:39 -0500

    corrected meme component

--- Tutorials/Scrimba_LearnReactFree_Projects/meme-generator/src/components/Meme.jsx
+++ Tutorials/Scrimba_LearnReactFree_Projects/meme-generator/src/components/Meme.jsx
@@ -3,8 +3,8 @@ import React from "react"
 export default function Meme(){
     return(
         <form className="meme-form">
-            <input className="meme-form--input" id="meme-form--row-1" type="text"></input>
-            <input className="meme-form--input" id="meme-form--row-2" type="text"></input>
+            <input className="meme-form--input" id="meme-form--row-1" type="text" placeholder="Top Text"></input>
+            <input className="meme-form--input" id="meme-form--row-2" type="text" placeholder="Bottom Text"></input>
             <input className="meme-form--button" id="meme-form--submit" type="submit" value="Get a new meme image"></input>
         </form>
     )
