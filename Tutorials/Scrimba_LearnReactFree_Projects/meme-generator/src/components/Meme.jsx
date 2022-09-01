ba8693eb6115f6764a324d4adac418f9ecd3c484
Paige <paige0011@gmail.com>
2022-08-31 19:07:30 -0500

    scrimba project 3 - added button functionality

--- Tutorials/Scrimba_LearnReactFree_Projects/meme-generator/src/components/Meme.jsx
+++ Tutorials/Scrimba_LearnReactFree_Projects/meme-generator/src/components/Meme.jsx
@@ -1,11 +1,26 @@
-import React from "react"
+import React, {useState} from "react"
+import memesData from "../memesData.js"
+
+
 
 export default function Meme(){
+    const[memeImage, setMemeImg] = useState("")
+
+    function getMemeImg(){
+        const index = Math.floor(Math.random() * 100)
+        const memeUrl = memesData.data.memes[index].url
+        setMemeImg(memeUrl)
+    }
+
     return(
-        <form className="meme-form">
-            <input className="meme-form--input" id="meme-form--row-1" type="text" placeholder="Top Text"></input>
-            <input className="meme-form--input" id="meme-form--row-2" type="text" placeholder="Bottom Text"></input>
-            <button className="meme-form--button" id="meme-form--submit">Get a new meme image</button>
-        </form>
+        <div className="meme">
+            <div className="meme-form">
+                <input className="meme-form--input" id="meme-form--row-1" type="text" placeholder="Top Text"></input>
+                <input className="meme-form--input" id="meme-form--row-2" type="text" placeholder="Bottom Text"></input>
+                <button className="meme-form--button" id="meme-form--submit" onClick={getMemeImg}>Get a new meme image</button>
+            </div>
+            <img className="meme-img" src={memeImage} />
+        </div>
+        
     )
 }
